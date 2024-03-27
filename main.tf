bd594a9401b1d7d328c2a11ecd4f412a3a05c8a0
Guilherme <guiramos027@gmail.com>
2024-03-27 18:06:20 -0300

    ci: terraform file to create AWS infrastructure as a code

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16"
+      version = "~> 5.42.0"
     }
   }
 
@@ -10,25 +10,119 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-west-2"
+  region     = "us-east-1"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
+resource "aws_iam_role" "beanstalk_service" {
+  name               = "beanstalk-service-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "elasticbeanstalk.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
 
-  tags = {
-    Name = "ExampleAppServerInstance"
-  }
+resource "aws_iam_role" "ec2_instance" {
+  name               = "ec2-instance-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2008-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_s3_bucket" "healphy-bucket" {
+  bucket = "healphy.app.bucket"
 }
 
 resource "aws_elastic_beanstalk_application" "healphy" {
-  name        = "healphy"
+  name        = "healphy-backend"
   description = "healphy-aws-elastic-beanstalk-backend"
 
   appversion_lifecycle {
     service_role          = aws_iam_role.beanstalk_service.arn
     max_count             = 128
     delete_source_from_s3 = true
+  }
+}
+
+resource "aws_elastic_beanstalk_environment" "healphy-env" {
+  name                = "healphy-env"
+  application         = aws_elastic_beanstalk_application.healphy.name
+  solution_stack_name = "64bit Windows Server 2022 v2.14.1 running IIS 10.0"
+
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "VPCId"
+    value     = var.vpc_id
+  }
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "IamInstanceProfile"
+    value     =  "elasticbeanstalk-ec2-role"
+  }
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "AssociatePublicIpAddress"
+    value     =  "True"
+  }
+ 
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "Subnets"
+    value     = join(",", var.public_subnets)
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:environment:process:default"
+    name      = "MatcherHTTPCode"
+    value     = "200"
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:environment"
+    name      = "LoadBalancerType"
+    value     = "application"
+  }
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "InstanceType"
+    value     = "t2.micro"
+  }
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "ELBScheme"
+    value     = "internet facing"
+  }
+  setting {
+    namespace = "aws:autoscaling:asg"
+    name      = "MinSize"
+    value     = 1
+  }
+  setting {
+    namespace = "aws:autoscaling:asg"
+    name      = "MaxSize"
+    value     = 2
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:healthreporting:system"
+    name      = "SystemType"
+    value     = "enhanced"
   }
 }

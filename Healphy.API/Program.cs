153c8643eceffcaf20c73c8d0d2444bc85608696
Guilherme <guiramos027@gmail.com>
2024-03-27 22:16:44 -0300

    feat: change program route to aws

--- Healphy.API/Program.cs
+++ Healphy.API/Program.cs
@@ -3,18 +3,23 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "healphy", Version = "v1" });
+}
+);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "healphyv1");
+    c.RoutePrefix = "swagger";
+
 }
+);
 
 app.UseHttpsRedirection();
 

dae1759e08c45f13f05b19b3bd1d06610cb0ce34
Guilherme <guiramos027@gmail.com>
2024-03-30 12:54:19 -0300

    feat: create database context

---
+++ Healphy.API/Data/HealphyDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Healphy.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Healphy.API.Data;
+public class HealphyDbContext : DbContext
+{
+    public HealphyDbContext(DbContextOptions<HealphyDbContext> options) : base(options) {}
+
+    public DbSet<Address> Address { get; set; }
+    public DbSet<Appointment> Appointment { get; set; }
+    public DbSet<Doctor> Doctor { get; set; }
+}
+

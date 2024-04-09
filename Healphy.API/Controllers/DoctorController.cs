9e0eae177f68201ce713a37a4d3b5cb9974f92d1
Guilherme <guiramos027@gmail.com>
2024-04-09 20:52:33 -0300

    feat: add get and post doctor api

---
+++ Healphy.API/Controllers/DoctorController.cs
@@ -0,0 +1,32 @@
+﻿using Healphy.API.Interfaces;
+using Healphy.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Healphy.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DoctorController : ControllerBase
+    {
+        public readonly IDoctorRepository _doctorRepository;
+
+        public DoctorController(IDoctorRepository doctorRepository)
+        {
+            _doctorRepository = doctorRepository;
+        }
+
+        [HttpGet]
+        [Route("doctors")]
+        public async Task<ActionResult<IEnumerable<Doctor>>> GetDoctors()
+        {
+            return Ok(await _doctorRepository.Get());
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<ActionResult<Doctor>> Register(Doctor doctorObj)
+        {
+            return Ok(await _doctorRepository.Create(doctorObj));
+        }
+    }
+}

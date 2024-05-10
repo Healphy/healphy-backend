a68092e8d0e272b902f1c1594f4a2c306f2bf8f9
Guilherme <guiramos027@gmail.com>
2024-05-09 21:59:11 -0300

    feat: doctor atributes ir nulable

--- Healphy.API/Models/Doctor.cs
+++ Healphy.API/Models/Doctor.cs
@@ -3,14 +3,14 @@
     public class Doctor
     {
         public int Id { get; set; }
-        public string DoctorsName { get; set; }
-        public string DoctorsLastName { get; set; }
-        public DateOnly BirthDate { get; set; }
-        public string Gender { get; set; }
-        public string Nationality { get; set; }
-        public string Email { get; set; }
-        public string Crm {  get; set; }
-        public string Cnpj { get; set; }
-        public string Speciality {  get; set; }
+        public string? DoctorsName { get; set; }
+        public string? DoctorsLastName { get; set; }
+        public DateOnly? BirthDate { get; set; }
+        public string? Gender { get; set; }
+        public string? Nationality { get; set; }
+        public string? Email { get; set; }
+        public string? Crm {  get; set; }
+        public string? Cnpj { get; set; }
+        public string? Speciality {  get; set; }
     }
 }

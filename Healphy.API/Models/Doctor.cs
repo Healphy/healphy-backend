f345eec0e6681b7d07acdef83d19dfe8abb91e37
Guilherme <guiramos027@gmail.com>
2024-06-13 21:32:54 -0300

    feat: add login feature

--- Healphy.API/Models/Doctor.cs
+++ Healphy.API/Models/Doctor.cs
@@ -9,6 +9,7 @@
         public string? Gender { get; set; }
         public string? Nationality { get; set; }
         public string? Email { get; set; }
+        public string? Password { get; set; }
         public string? Crm {  get; set; }
         public string? Cnpj { get; set; }
         public string? Speciality {  get; set; }
--- Healphy.API/Program.cs
+++ Healphy.API/Program.cs
@@ -2,7 +2,10 @@ using Healphy.API.Data;
 using Healphy.API.Interfaces;
 using Healphy.API.Mapping;
 using Healphy.API.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,11 +19,27 @@ builder.Services.AddCors();
 builder.Services.AddScoped<IDoctorRepository, DoctorRepository>();
 builder.Services.AddScoped<IAddressRepository, AddressRepository>();
 builder.Services.AddScoped<IAppointmentRepository, AppointmentRepository>();
-builder.Services.AddAutoMapper(typeof(EntitiesToDTOMappingProfile));
 
 string mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<HealphyDbContext>(options => options.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection)));
+
+builder.Services.AddAuthentication(option =>
+{
+    option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(option =>
+{
+    option.RequireHttpsMetadata = false;
+    option.SaveToken = true;
+    option.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("B4GSg^]]7g~ep7%*3Xob;dqM(Xlcy$")),
+        ValidateAudience = false,
+        ValidateIssuer = false
+    };
+});
 
 var app = builder.Build();
 
@@ -28,6 +47,7 @@ app.UseSwagger();
 app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
+app.UseCors(opcoes => opcoes.AllowAnyOrigin().AllowAnyHeader());
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
